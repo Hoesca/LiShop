e9e399b569649d51a48c73f571d25e91ca97caf6
HuoZhangqi <h947136@qq.com>
2019-03-15 14:19:31 +0800

    前端资源 && 基本布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -7,4 +7,7 @@
  */
 
 
-
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('test',function () {
+    return view('test');
 });
+
+Route::get('/', 'PagesController@root')->name('root');
